114a2240b43394b269877752d8b95263ae456af2
Dinesh Kumar Ilangovan <dinesh.ilangovan@tavant.com>
2018-10-14 18:17:09 +0530

    FIne tuned cost parameter to minimize gap between sequential actuations

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -71,8 +71,8 @@ class FG_eval {
 
 	// Minimize the value gap between sequential actuations.
 	for (int t = 0; t < N - 2; t++) {
-	  fg[0] += 25*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-	  fg[0] += 5*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+	  fg[0] += 50*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+	  fg[0] += 50*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
 	}
 
 
